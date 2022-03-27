a05a16cc6ac44add73990df259c68550bfa35586
Katya Freitag <k.freitag@mail.utoronto.ca>
2022-03-27 17:48:59 -0400

    Upload Challenge and analysis

--- PyPoll.py
+++ PyPoll.py
@@ -154,12 +154,10 @@ with open(file_to_save, "w") as txt_file:
     # After printing the final vote count to the terminal save the final vote count to the text file.
     txt_file.write(election_results)
     for candidate_name in candidate_votes:
-        # Retrieve vote count and percentage.
         votes = candidate_votes[candidate_name]
         vote_percentage = float(votes) / float(total_votes) * 100
         candidate_results = (
             f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-
         # Print each candidate's voter count and percentage to the terminal.
         print(candidate_results)
         #  Save the candidate results to our text file.
